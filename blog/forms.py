1d6471baa39102d67eefb41f0d1babfe4996554a
Scott McAndrew <scottymcandrew@outlook.com>
2020-02-24 18:27:22 +0000

    Added comments functionality. Had to add trailing / in urls since adding comments failed

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
@@ -8,3 +9,11 @@ class EmailPostForm(forms.Form):
     # Widget determines how the field is rendered in HTML. Default is <input type="text">
     # Textarea widget changes HTML to <textarea> element instead
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):  # ModelForm builds the form dynamically based on the Comment model
+    class Meta:
+        # Here we specify the model (Comment) to build the form
+        model = Comment
+        # Django builds the form based on all model fields by default. Use fields to limit this
+        fields = ('name', 'email', 'body')
--- blog/models.py
+++ blog/models.py
@@ -42,3 +42,22 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+
+class Comment(models.Model):
+    # Many to one relationship since many comments can be associated with a single post
+    # We name the relation attribute and can refer to it e.g. comment.post or post.comments.all()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # Active allows us to deactivate inappropriate comments
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
