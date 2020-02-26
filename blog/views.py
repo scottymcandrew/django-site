aecc6612ac75519f34d0e98bc2d413c044025d85
Scott McAndrew <scottymcandrew@outlook.com>
2020-02-26 17:52:42 +0000

    Enhanced search with weighting

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
 from taggit.models import Tag
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
 
@@ -110,9 +110,13 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            # SearchRank - order the results by relevancy
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
 
     return render(request, 'blog/post/search.html',
                   {'form': form,
